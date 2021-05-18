aa98b5d43c326d29bcb0318ea7e34444818e89ce
566810 <timranzijn1@gmail.com>
2021-05-18 15:51:46 +0200

    Iban generator half af

---
+++ src/main/java/BankAccountInserter.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class BankAccountInserter implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
--- src/main/java/io/swagger/api/AccountsController/IbanGenerator.java
+++ src/main/java/io/swagger/api/AccountsController/IbanGenerator.java
@@ -5,13 +5,17 @@ import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.OptionalLong;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class IbanGenerator implements IdentifierGenerator {
 
     private static final String COUNTRY_CODE = "NL";
     private static final String CONTROL_NUMBER = "00";
     private static final String BANK_CODE = "INHO";
-    private static final int AMOUNT_OF_MAX_DIGITS_IN_IBAN = 34;
+    private static final int AMOUNT_OF_MAX_DIGITS_IN_ACCOUNT_NUMBER = 26;
 
 
     @Override
@@ -22,7 +26,22 @@ public class IbanGenerator implements IdentifierGenerator {
                         .getIdentifierPropertyName(),
                 obj.getClass().getSimpleName());
 
-        return "ff";
+        //haalt alle id's  op die beginnen met NL00INHO
+        Stream ids = session.createQuery(query).stream().filter(x -> x.toString().contains(COUNTRY_CODE + CONTROL_NUMBER + BANK_CODE));
+
+
+        //haalt de letters (NL00INHO van de strings af, converteert de resterende string (bestaande uit cijfers) naar een long, en kiest de hoogste waarde uit.
+        OptionalLong max = ids.mapToLong(x -> {
+            return Long.parseLong(x.toString().substring(8));
+        }).max();
+
+        // als er geen geldige ibans zijn doe dit
+        if (!max.isPresent()){
+            return "ff";
+        }
+
+        //anders return max
+        return COUNTRY_CODE + CONTROL_NUMBER + BANK_CODE + max;
 
 
 
