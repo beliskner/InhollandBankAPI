1e230a913dd0c68f4d40bfc742ca2fafc2643631
566810 <timranzijn1@gmail.com>
2021-05-20 16:31:42 +0200

    1. missing repository for accounts added

--- src/main/java/io/swagger/service/accounts/AccountsService.java
+++ src/main/java/io/swagger/service/accounts/AccountsService.java
@@ -1,6 +1,4 @@
 package io.swagger.service.accounts;
-
-
 import io.swagger.model.Account;
 import io.swagger.model.BaseModels.BaseAccount.AccountTypeEnum;
 import io.swagger.model.BaseModels.BaseAccount.StatusEnum;
@@ -8,7 +6,6 @@ import io.swagger.model.DTO.AccountDTO.RequestBodyAccount;
 import io.swagger.repository.AccountsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.util.Iterator;
@@ -21,16 +18,15 @@ import java.util.stream.StreamSupport;
 
 @Service
 public class AccountsService {
-
     @Autowired
     private AccountsRepo accountsRepo;
-
 
     public Account addAccount(@Valid RequestBodyAccount body) {
         StatusEnum status = body.getStatus();
         AccountTypeEnum accountType = body.getAccountType();
         Integer holderId = body.getHolderId();
         BigDecimal maxTransfer =  body.getMaxTransfer();
+        //TODO: DEFAULT WAARDES IN MODEL ZETTEN.
         if (body.getMaxTransfer() == null){maxTransfer = new BigDecimal("500");}
         BigDecimal minBalance = body.getMinBalance();
         if (body.getMinBalance() == null){minBalance = new BigDecimal("-500");}
@@ -44,16 +40,6 @@ public class AccountsService {
         acc.setMaxTransfer(maxTransfer);
 
         return accountsRepo.save(acc);
-
-
-
-/*        ReturnBodyAccount rba = new ReturnBodyAccount();
-        rba.setBalance(acc.getBalance());
-        rba.setHolderId(acc.getHolderId());
-        rba.setIban(acc.getIban());
-        rba.setAccountType(acc.getAccountType());
-        rba.setMinBalance(acc.getMinBalance());
-        rba.setStatus(acc.getStatus());*/
 
     }
     public void updateAccount(@Valid RequestBodyAccount body) {
