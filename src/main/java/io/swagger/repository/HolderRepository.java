8aeaad9558039c5b83639eb53ed55141db4df3e4
rene-steneker <rene.steneker@gmail.com>
2021-05-26 14:18:14 +0200

    Added GetHolderById to repo and service

--- src/main/java/io/swagger/repository/HolderRepository.java
+++ src/main/java/io/swagger/repository/HolderRepository.java
@@ -2,15 +2,17 @@ package io.swagger.repository;
 
 
 import io.swagger.model.Holder;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface HolderRepository extends CrudRepository<Holder, Long> {
     Holder findByEmail(String email);
 
     List<Holder> findAll();
+
+    Optional<Holder> findById(Long id);
 }
